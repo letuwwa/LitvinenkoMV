8e2932116b63a679890160affb0c6d629af1bd59
letuwwa <letuwwa@gmail.com>
2021-02-04 23:36:05 +0300

    commit #11

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
+from django.urls import reverse
 
-# Create your models here.
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=32, blank=False, verbose_name='Имя')
+    text = models.TextField(max_length=1024, blank=False, verbose_name='Текст')
+    email = models.EmailField(max_length=256, blank=False, verbose_name='Почта для ответа')
+    date = models.DateTimeField(default=datetime.now, blank=False)
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from .models import Feedback
+from .forms import FeedbackForm
+from django.views.generic import (
+    CreateView,
+)
 
 
 def index(request):
     return render(request, 'base.html', context={})
 
 
-def contacts(request):
-    return render(request, 'other/contacts.html', context={})
+class FeedbackCreate(CreateView):
+    template_name = 'other/contacts.html'
+    model = Feedback
+    form_class = FeedbackForm
