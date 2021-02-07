b161b8947b9d3e0481e456a47e55cdac5b3d6d55
letuwwa <letuwwa@gmail.com>
2021-02-07 13:23:48 +0300

    commit #14 response page finished

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('contacts/', views.FeedbackCreate.as_view(), name='contacts'),
+    path('contacts_r/', views.feedback_response, name='contacts_r'),
     path('', views.index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -12,10 +12,14 @@ def index(request):
     return render(request, 'base.html', context={})
 
 
+def feedback_response(request):
+    return render(request, 'other/contacts_response_success.html', context={})
+
+
 class FeedbackCreate(CreateView):
     template_name = 'other/contacts.html'
     model = Feedback
     form_class = FeedbackForm
 
     def get_success_url(self):
-        return reverse_lazy('contacts')
+        return reverse_lazy('contacts_r')
